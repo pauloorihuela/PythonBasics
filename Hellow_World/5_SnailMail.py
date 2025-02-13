7cac198616bb8de1aad5ec1ea2abf35ca41c021a
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-13 22:00:19 +0100

    Create 5_SnailMail.py
    A basic exercise from codedex.

---
+++ Hellow_World/5_SnailMail.py
@@ -0,0 +1,16 @@
+# Write code below 💖
+
+# Today's date.
+print("13/02/2025")
+
+# How you are feeling right now.
+print("Happy")
+
+# What you want to accomplish by learning to code.
+print("Making apps and webs")
+
+# A little message to your older, wiser, and programmer self.
+print("All the path that we passed, it's a way to grow up in all knowledge of programming")
+
+# Your favorite emoji to spice things up!print("")
+print("🫶")
