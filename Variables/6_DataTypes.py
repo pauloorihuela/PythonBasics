24cba78b76e324c1822244c95e621d8cdbdcd394
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-13 22:09:41 +0100

    Create 6_DataTypes.py
    A few kind of types of variables from codedex.

---
+++ Variables/6_DataTypes.py
@@ -0,0 +1,17 @@
+# Write code below 💖
+
+# Integer
+year = 2023
+age = 32
+
+# Float
+pi = 3.14159
+meal_cost = 12.99
+
+#String
+message = "good nite"
+username = '@snoopdogg'
+
+#Boolean
+late_to_class = False
+cranky = True
