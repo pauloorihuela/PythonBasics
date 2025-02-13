11d3d30e82b287322d13dcc15dd545f8c98b2543
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-13 22:16:25 +0100

    Create 7_temperature.py
    An exercise converting an input Fahrenheit to Celsius.

---
+++ Variables/7_temperature.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+
+fahrenheit=int(input("Insert the temperature in Fahrenheit "))
+celsius=float()
+
+celsius = (fahrenheit-32)/1.8
+
+print("The temperature in Celsius is ", celsius)
