01ea82afb8b9bee970fd2bfdb6fb9d80d7737e25
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-13 20:43:25 +0100

    Create 4_initials.py
    A simple exercise from codedex using comentaries and print.

---
+++ Hellow_World/4_initials.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+
+
+#This is a fun comentary about me ...!!!
+
+print("PPPPPPP  OOOOOOOOO") 
+print("P     P  O       O") 
+print("P     P  O       O") 
+print("PPPPPPP  O       O")
+print("P        O       O")
+print("P        O       O")
+print("P        OOOOOOOOO")
