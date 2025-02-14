69f2a0a64c0dafd563a3ae4921160ddff6986440
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-14 22:06:46 +0100

    Create 9_PythagoreanTheorem.py
    An exercise showing the pythagorean theorem from codedex.

---
+++ Variables/9_PythagoreanTheorem.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+
+a=int(input("Please, input one side of the triangle "))
+b=int(input("Please, input the second side of the triangle "))
+
+c=(a**2+b**2)**0.5
+
+print("The hypotenuse is ", c)
