993d74bb5a282295d564e7a6ea52fa941a687c2f
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-16 14:53:45 +0100

    Create 13_pHLevels.py
    An exercise from codedex using if, elif, and else.

---
+++ ControlFlow/13_pHLevels.py
@@ -0,0 +1,14 @@
+# Write code below 💖
+
+# Write code below 💖
+
+import random
+
+pH = random.randint(0, 14)   # Generates a random number that's either 0 or 1
+
+if pH > 7:
+  print('Basic')
+elif pH < 7:
+    print("Acidic")
+else:
+  print('Neutral')
