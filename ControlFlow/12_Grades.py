4876fc013d384ac92563a26c4015bac1d4515657
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-16 14:36:26 +0100

    Create 12_Grades.py
    An exercise from codedex using if an else.

---
+++ ControlFlow/12_Grades.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+
+
+import random
+
+grade = random.randint(0, 100)   # Generates a random number that's either 0 or 1
+
+if grade > 54:
+  print('You passed.')
+else:
+  print('You failed.')
