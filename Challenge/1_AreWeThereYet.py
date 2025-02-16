3f8a8b965651c844351f8c7d00765c9ee6f6a113
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-16 19:05:37 +0100

    Create 1_AreWeThereYet.py
    First challenge from codedex.

---
+++ Challenge/1_AreWeThereYet.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+
+answer = str(input("Are we there yet? "))
+answer = answer.lower()
+
+while answer!="yes":
+  answer = str(input("Are we there yet? "))
+  answer = answer.lower()
+
+print("Good")
