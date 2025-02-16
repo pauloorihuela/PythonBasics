1b6d1b75c95486c892df43508b495cea3a93a7a6
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-16 15:25:06 +0100

    Create 14_Magic8Balls.py
    A basic exercise from codedex about a simulation of magic 8 balls.

---
+++ ControlFlow/14_Magic8Balls.py
@@ -0,0 +1,36 @@
+# Write code below 💖
+
+
+import random
+
+answer = random.randint(0, 14)   # Generates a random number that's either 0 or 1
+
+question = str(input("Input your question "))
+
+print(question)
+if answer == 0:
+    print('Yes - definitely.')
+elif answer == 1:
+    print("It is decidedly so.")
+elif answer == 2:
+    print("Reply hazy, try again.")
+elif answer == 3:
+    print("Ask again later.")
+elif answer == 4:
+    print("Better not tell you now.")
+elif answer == 5:
+    print("Better not tell you now.")
+elif answer == 6:
+    print("My sources say no.")
+elif answer == 7:
+    print("Outlook not so good.")
+elif answer == 8:
+    print("Very doubtful.")
+
+
+
+  
+
+
+
+
